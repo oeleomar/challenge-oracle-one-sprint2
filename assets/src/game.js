1e7f1f8c0aa6391c35f4dfdeb517eb6d7be1e221
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-09-08 16:04:04 -0400

    Add the final code

--- assets/src/game.js
+++ assets/src/game.js
@@ -1,6 +1,15 @@
 const textContainer = document.querySelector("#text-container");
+const errorContainer = document.querySelector("#error-container");
+const resultContainer = document.querySelector("#result-container");
+const imageContainer = document.querySelectorAll('.hidden');
 const words = JSON.parse(localStorage.getItem('data'));
+
+console.log(imageContainer);
+
 let counter = 0;
+const usedLetters = [];
+const errorLetters = [];
+const correctLetters = [];
 
 const initGame = (e) => {
   if(e.keyCode > 90 || e.keyCode < 60) return;
@@ -25,20 +34,64 @@ const placeLetters = (word) => {
 }
 
 const letterVerify = (letter) => {
-  for(let i = 0; i < word.length; i++){
-    console.log(word[i], letter);
-    if(letter === word[i]){
-      textContainer.childNodes[i + 1].textContent = letter
-      counter ++
-      console.log(counter)
-      checkWinner();
-    }
+  const validator = verifyLetter(letter);
+  if(validator) return;
 
+  usedLetters.push(letter);
+  for(let i = 0; i < word.length; i++){
+    console.log('Começei o laço')
+    if(letter === word[i]){
+      console.log('Entrei');
+      textContainer.childNodes[i + 1].textContent = letter;
+      counter ++;
+      correctLetters.push(letter);
+    }  
+  }
+  
+  checkWinner();
+  usedLetters.map(val => {
+    if(correctLetters.includes(val)) return;
+    if(errorLetters.includes(val)) return;
+    errorLetters.push(val);
+    showIncorrectLetters(val);
+  })
+}
+
+const verifyLetter = (letter) => {
+  return usedLetters.includes(letter);
+}
+
+
+const checkWinner = () => {
+  if(word.length <= correctLetters.length){
+    const span = document.createElement('span');
+    span.classList.add('winner');
+    span.textContent = 'Voce conseguiu, parabens !! 😀😀';
+    resultContainer.appendChild(span);
+    document.removeEventListener('keydown', initGame);
+  }
+
+  if(errorLetters.length >= 5) {
+    const span = document.createElement('span');
+    span.classList.add('looser');
+    span.textContent = 'Voce perdeu, tente novamente. 🥺🥺';
+    resultContainer.appendChild(span);
+    document.removeEventListener('keydown', initGame);
   }
 }
 
-const checkWinner = () => {
+const showIncorrectLetters = (val) => {
+  const span = document.createElement('span');
+  span.classList.add('errors');
+  span.textContent = val;
+  errorContainer.appendChild(span);
+  showImage();
+}
 
+const showImage = () => {
+  const img = imageContainer[errorLetters.length - 1];
+  img.classList.remove('hidden');
+  img.classList.add('showed');
 }
 
 const word = sortWord(words);
